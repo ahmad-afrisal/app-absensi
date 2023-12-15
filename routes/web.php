1bc7bfce3e190ae52897705743ebedc76628da41
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-12-15 16:50:11 +0800

    Setup Template

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+Route::get('/app', function () {
+    return view('layouts.app');
 });
 
 Route::get('/dashboard', function () {
