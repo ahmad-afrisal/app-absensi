015d5474107f99539193ccdd1e02e5485c819942
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-12-24 11:32:57 +0800

    Make Migration

---
+++ database/migrations/2023_12_18_112918_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->string('nis')->primary();
+            $table->foreignId('user_id')->constrained();
+            $table->string('nisn');
+            $table->string('fullname');
+            $table->boolean('gender');
+            $table->string('level');
+            $table->text('address');
+            $table->date('date_of_birth');
+            $table->string('picture');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
